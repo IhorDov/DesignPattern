e78d6dbf15c91fedb37ade5db96dfeae0b260e71
Ihor Dovhun <15272@student.eadania.dk>
2022-04-05 15:30:37 +0200

    first commit

--- ComponentPattern/Fire.cs
+++ ComponentPattern/Fire.cs
@@ -11,41 +11,37 @@ namespace DesignPattern
 
         private Vector2 velocity;
 
-        public Vector2 Velocity { get; set; }
+        //public Vector2 Velocity { get; set; }
 
         public Fire()
         {
             this.speed = 500;
-            
-            this.Velocity = velocity; // new Vector2(0, -1);
-        }
-        public override void Update()
-        {
+
+            //this.velocity = velocity; // new Vector2(0, -1);
+
             if (velocity.X > 0)
             {
-                this.velocity = new Vector2(0, -1);
-                Move();
+                velocity = new Vector2(1, 0);
             }
             else if (velocity.X < 0)
             {
-                this.velocity = new Vector2(0, 1);
-                Move();
+                velocity = new Vector2(-1, 0);
             }
             else if (velocity.Y < 0)
             {
-                this.velocity = new Vector2(0, -1);
-                Move();
+                velocity = new Vector2(0, -1);
             }
             else if (velocity.Y > 0)
             {
-                this.velocity = new Vector2(0, -1);
-                Move();
+                velocity = new Vector2(0, 1);
             }
-
-            //Move();
+        }
+        public override void Update()
+        { 
+            Move(velocity);
         }
 
-        private void Move()
+        private void Move(Vector2 velocity)
         {
             if (velocity != Vector2.Zero)
             {
@@ -56,7 +52,40 @@ namespace DesignPattern
 
             GameObject.Transform.Translate(velocity * GameWorld.DeltaTime);
 
+            //if (velocity.X > 0)
+            //{
+            //    velocity = new Vector2(1, 0);
+            //}
+            //else if (velocity.X < 0)
+            //{
+            //    velocity = new Vector2(-1, 0);
+            //}
+            //else if (velocity.Y < 0)
+            //{
+            //    velocity = new Vector2(0, -1);
+            //}
+            //else if (velocity.Y > 0)
+            //{
+            //    velocity = new Vector2(0, 1);
+            //}
+            
+
             if (GameObject.Transform.Position.Y < 0)
+            {
+                GameWorld.Instance.Destroy(this.GameObject);
+            }
+
+            if (GameObject.Transform.Position.X < 0)
+            {
+                GameWorld.Instance.Destroy(this.GameObject);
+            }
+
+            if (GameObject.Transform.Position.Y > GameWorld.Instance.GraphicsDevice.Viewport.Height)
+            {
+                GameWorld.Instance.Destroy(this.GameObject);
+            }
+
+            if (GameObject.Transform.Position.X > GameWorld.Instance.GraphicsDevice.Viewport.Width)
             {
                 GameWorld.Instance.Destroy(this.GameObject);
             }
